3a3c49107e89497c9ff3556bb3be92754012195e
Crash-afftar <sashakhymenko88@gmail.com>
2023-05-07 16:17:45 +0300

    rabbit race 1

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -5,7 +5,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
         <div className='Button-container'>
             {options.map(option => (
                 <button key={option} onClick={() => onLeaveFeedback(option)} className="Button">
-                    {option}
+                    {option.charAt(0).toUpperCase() + option.slice(1)}
                 </button >
             ))}
         </div>
